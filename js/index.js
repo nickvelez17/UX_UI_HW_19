58752ce23eb5872b372185916e606380b9a742a9
MainlyGG <111713014+MainlyGG@users.noreply.github.com>
2023-02-07 16:29:53 -0500

    PortfolioClickable

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const faders = document.querySelectorAll('.fade');
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -300px 0px"
+};
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+     appearOnScroll
+     ) {
+        entries.forEach(entry =>{
+          if(!entry.isIntersecting) {
+            return;
+          } else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+          }
+        
+        });
+     },
+      appearOptions);
+      
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
